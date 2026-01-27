da5a98f7e399f6dcc8672eca1946960c8d8982e2
silverwebbuzz <silverwebbuzz@gmail.com>
2026-01-27 15:21:26 +0530

    URL change

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_USER', 'silverwebbuzz_in_pcm');
 define('DB_PASS', 'PCM@swb$5555');
 
 // Base URL (update if app is in a subfolder)
-define('BASE_URL', '/');
+define('BASE_URL', '/pcm/');
 
 // File upload configuration
 define('UPLOAD_DIR', __DIR__ . '/uploads');
